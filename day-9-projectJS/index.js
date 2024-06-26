f781c5c7ed17717ea11cb1af6d5284f9c1b99a91
Lima Junior <fjdlimajunior@gmail.com>
2024-06-26 15:42:53 -0300

    create api and implementing

---
+++ day-9-projectJS/index.js
@@ -0,0 +1,5 @@
+//O QUE SÃO FUNÇÕES E POR QUE SÃO IMPORTANTES! AULA 25/06/2024
+//EXTRUTURA DE UMA FUNÇÃO
+const mensagemElemento = document.getElementById('mensagem');
+
+mensagemElemento.textContent = 'Olá, Mundo! Este é meu primeiro Hello World com JavaScript!';
